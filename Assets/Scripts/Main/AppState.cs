202a9bff77edb7d385d1dfa4d74662bd5a1f075d
EdwardBrave <edward.brave45@gmail.com>
2022-02-10 19:53:04 +0200

    creaded abstract AppState

---
+++ Assets/Scripts/Main/AppState.cs
@@ -0,0 +1,55 @@
+﻿using Entitas;
+
+public abstract class AppState: Feature
+{
+    ////////////////////////////////////////////////////////////////////
+    #region Data
+
+    protected readonly Contexts contexts;
+    
+    #endregion
+    ////////////////////////////////////////////////////////////////////
+    #region Interface
+    
+    public AppState(Contexts newContexts)
+    {
+        contexts = newContexts;
+        var systems = GetSystems(contexts);
+        foreach (var system in systems)
+        {
+            Add(system);
+        }
+    }
+    
+    protected abstract ISystem[] GetSystems(Contexts contexts);
+    
+    protected virtual void Init() { }
+    protected virtual void Activate() { }
+    protected virtual void Deactivate() { }
+    protected virtual void Destroy() { }
+
+    #endregion
+    ////////////////////////////////////////////////////////////////////
+    #region Feature implementation
+    
+    public sealed override Systems Add(ISystem system) => base.Add(system);
+
+    public sealed override void Initialize()
+    {
+        Init();
+        base.Initialize();
+    }
+
+    public sealed override void Execute() => base.Execute();
+    
+    public sealed override void Cleanup() => base.Cleanup();
+    
+    public sealed override void TearDown()
+    {
+        base.TearDown();
+        Destroy();
+    }
+
+    #endregion
+    ////////////////////////////////////////////////////////////////////
+}
