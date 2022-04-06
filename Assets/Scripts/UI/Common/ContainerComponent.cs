005f516f2c30a68e55bcf27a44d511b1ce1f407c
EdwardBrave <edward.brave45@gmail.com>
2022-04-06 11:19:56 +0300

    disabled ContainerComponent drawing

--- Assets/Scripts/UI/Common/ContainerComponent.cs
+++ Assets/Scripts/UI/Common/ContainerComponent.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
 using Entitas;
+#if (!ENTITAS_DISABLE_VISUAL_DEBUGGING && UNITY_EDITOR)
+using Entitas.VisualDebugging.Unity;
+#endif
 using UnityEngine;
 
 namespace UI.Common
 {
+#if (!ENTITAS_DISABLE_VISUAL_DEBUGGING && UNITY_EDITOR)
+    [DontDrawComponent]
+#endif
     [Ui]
     public class ContainerComponent : UiComponent
     {
