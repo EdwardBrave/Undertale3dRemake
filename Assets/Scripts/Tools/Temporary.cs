8449a8741a4ebaf4ec96cd148563fc250cd2c4be
EdwardBrave <edward.brave45@gmail.com>
2022-04-09 21:55:29 +0300

    added Temporary asset class

---
+++ Assets/Scripts/Tools/Temporary.cs
@@ -0,0 +1,21 @@
+﻿namespace Tools
+{
+    public enum TemporaryStatus
+    {
+        Started,
+        Stay,
+        Ended
+    }
+    
+    public class Temporary<T>
+    {
+        public readonly T Data;
+        public TemporaryStatus Status;
+
+        public Temporary(T data, TemporaryStatus status = TemporaryStatus.Started)
+        {
+            Data = data;
+            Status = status;
+        }
+    }
+}
